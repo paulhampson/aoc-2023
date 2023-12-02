9ae1a7da94d51dd840c221e07a489ad15b527f48
Paul Hampson <528002+paulhampson@users.noreply.github.com>
2023-12-02 22:13:12 +0000

    Day1

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn digit_string_check( s: &str ) -> Result<char, char> {
+    let check_items = [
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9')
+        ];
+    for (check_string, value) in check_items {
+        if s.starts_with(check_string) {
+            return Ok(value);
+        }
+    }
+    return Err('X');
+}
+
+fn day1() {
+    println!("Day 1");
+    let mut sum_of_numbers = 0;
+    if let Ok(lines) = read_lines("inputs/day1.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            let mut digits_in_string = String::new();
+            if let Ok(ip) = line {
+                for (i, c) in ip.chars().enumerate() {
+                    let (_, remaining_string) = ip.split_at(i);
+
+                    // check for either numeric digits or string digits
+                    if c.is_digit(10) {
+                        digits_in_string.push(c);
+                    } else if let Ok(digit) = digit_string_check(remaining_string) {
+                        digits_in_string.push(digit);
+                    }
+                }
+            }
+
+            // now get the first and last digit and sum
+            let first_digit = digits_in_string.chars().nth(0).unwrap();
+            let last_digit = digits_in_string.chars().nth_back(0).unwrap();
+            let mut number_from_string = first_digit.to_string();
+            number_from_string.push(last_digit);
+            let n: i32 = number_from_string.parse().unwrap();
+            sum_of_numbers += n;
+        }
+    }
+    println!("{}", sum_of_numbers);
+}
+
+fn main() {
+    day1();
+}
